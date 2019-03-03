823693c1f60862bb89452861dffcab20a28f41f8
Virginia Dooley <vcdooley@gmail.com>
2019-03-03 17:03:56 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,22 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = Comment.new
+  end
+
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    @comment = Comment.create(comment_params)
+    # @comment.user.build
+    redirect_to comment_path(@comment)
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_ids:[], user_attributes:[:username, :email])
   end
 end
