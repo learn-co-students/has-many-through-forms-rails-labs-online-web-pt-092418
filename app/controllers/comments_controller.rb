11690d1c1105dca883a1df03522c8519bed27606
Anuj Panchal <anujpan1@gmail.com>
2019-03-20 12:49:09 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,18 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = Comment.new
+  end
+  
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    @comment = Comment.create(comment_params)
+    redirect_to @comment.post
   end
 
+  def show
+    @comment = Comment.find(params[:id])
+  end
+  
   private
 
   def comment_params
